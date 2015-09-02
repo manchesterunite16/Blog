44845405e7aaa8e30950db17086f075e5f58bf64
shipeng19901010 <19901010shipeng>
2015-09-02 15:44:41 +0800

    python
    python code

--- 39.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-response = urllib.request.urlopen('http://placekitten.com/g/500/900')
-html = response.read()
-print(html)
-
-with open('cat_500_500.jpg', 'wb') as f:
-    f.write(html)
